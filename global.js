fbd2e3d5870a2caa234ea437cb84eee47c354a57
nicolas toon <nicolastoon05@gmail.com>
2025-04-16 13:06:56 -0700

    fixing broke nav menu

--- global.js
+++ global.js
@@ -21,8 +21,12 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"                 
+    : "/portfolio/";
+
 for (let p of pages) {
-    let url = p.url;
+    let url = !p.url.startsWith('http') ? BASE_PATH + p.url : p.url;
     let title = p.title;
     nav.insertAdjacentHTML('beforeend', `<a href='${url}'>${title}</a>`)
 }
