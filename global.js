700424ab665f8f3e0fca268bac57ac4db11dd3e0
nicolas toon <nicolastoon05@gmail.com>
2025-04-16 19:28:59 -0700

    creating the shadows (dark mode)

--- global.js
+++ global.js
@@ -4,12 +4,6 @@ function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector))
 }
 
-// let navLinks = $$('nav a')
-// let currentLink = navLinks.find(
-//     (a) => a.host === location.host && a.pathname === location.pathname,
-// );
-// currentLink?.classList.add('current');
-
 let pages = [
     {url: '', title: 'home'},
     {url: 'resume/', title: 'resume'},
@@ -17,16 +11,53 @@ let pages = [
     {url: 'https://github.com/nicolastoon', title: 'github'},
     {url: 'contact/', title: 'contact'},
 ]
-
 let nav = document.createElement('nav');
 document.body.prepend(nav);
-
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"                 
     : "/portfolio/";
-
 for (let p of pages) {
     let url = !p.url.startsWith('http') ? BASE_PATH + p.url : p.url;
     let title = p.title;
-    nav.insertAdjacentHTML('beforeend', `<a href='${url}'>${title}</a>`)
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
 }
+
+let default_option = matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
+let secondary = default_option === 'dark' ? 'light' : 'dark';
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+        theme:
+        <select>
+            <option>${default_option} (system default)</option>
+            <option>${secondary}</option>
+        </select>
+    </label>`,
+);
+
+let select = document.querySelector('select');
+select.addEventListener('input', function (event) {
+    let theme = event.target.value.startsWith(default_option) ? default_option : secondary;
+    console.log('color scheme changed to', theme);
+    document.documentElement.style.setProperty('color-scheme', theme);
+    localStorage.colorScheme = theme;
+});
+if (localStorage.colorScheme) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+}
+
+
+
