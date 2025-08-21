33697a84625db9485865dc98a68056ba79a3e530
nicolas toon <nicolastoon05@gmail.com>
2025-08-20 23:42:23 -0700

    trying to fix picture

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,9 +17,9 @@ export default function Header() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className="lucide lucide-map-pin-icon lucide-map-pin"
           >
             <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
